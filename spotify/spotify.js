e55bdaee00d858104f575d33bcb8c5078b52c69e
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2023-12-14 11:56:06 +0000

    Creazione card artista

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -43,6 +43,49 @@ function searchArtists(artistName) {
 
 getTokenBearer();
 
-setTimeout(() => {
-    searchArtists('salmo')
-}, 1000)
+function searchArtistClick() {
+    // recuperiamo dalla input il nome dell'artista da cercare
+    let nomeArtista = document.getElementById('input-name').value;
+
+    // eseguiamo la funzione che invia la chiamata alla API di spotify
+    searchArtists(nomeArtista);
+}
+
+function createArtistCard(artist) {
+    /*
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    */
+    let card = document.createElement('div');
+    card.className = 'card';
+    let image = document.createElement('img');
+    image.className = 'card-img-top';
+    image.src = artis.images[1].url;
+    image.alt = artist.name;
+    card.appendChild(image);
+    let divBody = document.createElement('div');
+    divBody.className = 'card-body';
+    let title = document.createElement('h5');
+    title.className = 'card-title';
+    title.innerText = artist.name;
+    divBody.appendChild(title);
+    let text = document.createElement('p');
+    text.className = 'card-text';
+    /* text.innerText = artist.genres.reduce((generi, genre) => {
+        return generi + ' - ' + genre;
+    }) */
+    let generi = '';
+    artist.genres.forEach(genere => {
+        generi = generi + ' - ' + genere;
+    });
+    text.innerText = generi;
+    divBody.appendChild(text);
+    card.appendChild(divBody);
+    return card;
+}
